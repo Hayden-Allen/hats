5ea5cff04725719442b1a6ed984bad8f12f8280c
Hayden Allen <hsa190003@utdallas.edu>
2024-01-21 22:58:52 -0600

    direction changes

--- src/direction.h
+++ src/direction.h
@@ -20,64 +20,64 @@ namespace hats
 	public:
 		constexpr direction() {}
 		template<typename X, typename Y, typename Z>
-		constexpr direction(const X x, const Y y, const Z z) :
+		constexpr direction(X const x, Y const y, Z const z) :
 			vec_base<SPACE>(x, y, z, 0.f)
 		{
 			normalize();
 		}
-		explicit constexpr direction(const vec_base<SPACE>& v) :
+		explicit constexpr direction(vec_base<SPACE> const& v) :
 			vec_base<SPACE>(v.x, v.y, v.z, 0.f)
 		{
 			normalize();
 		}
 	public:
 		template<typename T>
-		constexpr vec<SPACE> operator*(const T s) const;
-		constexpr direction<SPACE> operator-() const
+		vec<SPACE> constexpr operator*(T const s) const;
+		direction<SPACE> constexpr operator-() const
 		{
 			return direction<SPACE>(-x, -y, -z);
 		}
-		constexpr direction<SPACE>& operator=(const direction<SPACE>& o)
+		direction<SPACE> constexpr& operator=(direction<SPACE> const& o)
 		{
 			vec_util::copy(e, o.e);
 			return *this;
 		}
-		constexpr bool operator==(const direction<SPACE>& o) const
+		bool constexpr operator==(direction<SPACE> const& o) const
 		{
 			return vec_base<SPACE>::is_equal(o);
 		}
-		constexpr bool operator!=(const direction<SPACE>& o) const
+		bool constexpr operator!=(direction<SPACE> const& o) const
 		{
 			return vec_base<SPACE>::is_not_equal(o);
 		}
 	public:
-		constexpr f32 dot(const direction<SPACE>& o) const
+		constexpr f32 dot(direction<SPACE> const& o) const
 		{
 			return vec_util::dot(e, o.e);
 		}
-		constexpr f32 angle_to(const direction<SPACE>& o) const
+		constexpr f32 angle_to(direction<SPACE> const& o) const
 		{
 			return std::acos(dot(o));
 		}
 		template<space TO>
-		constexpr direction<TO> transform_copy(const tmat<SPACE, TO>& m) const
+		direction<TO> constexpr transform_copy(tmat<SPACE, TO> const& m) const
 		{
 			f32 t[4] = { 0.f };
 			vec_util::transform(t, m.e, e);
 			return direction<TO>(t[0], t[1], t[2]);
 		}
-		constexpr direction<SPACE>& transform(const tmat<SPACE, SPACE>& m)
+		direction<SPACE> constexpr& transform(tmat<SPACE, SPACE> const& m)
 		{
 			vec_util::transform(e, m.e, e);
 			return *this;
 		}
-		constexpr direction<SPACE>& cross(const direction<SPACE>& o)
+		direction<SPACE> constexpr& cross(direction<SPACE> const& o)
 		{
 			vec_util::cross(e, e, o.e);
 			normalize();
 			return *this;
 		}
-		constexpr direction<SPACE> cross_copy(const direction<SPACE>& o) const
+		direction<SPACE> constexpr cross_copy(direction<SPACE> const& o) const
 		{
 			f32 t[3] = { 0.f };
 			vec_util::cross(t, e, o.e);
@@ -88,15 +88,22 @@ namespace hats
 			printf("dir<%d>\t{ %06f\t%06f\t%06f }\n", SPACE, x, y, z);
 		}
 	private:
-		constexpr void normalize()
+		void constexpr normalize()
 		{
 			const f32 length2 = vec_util::length2(e);
 			// special "null direction" case
 			if (length2 == 0.f)
 				return;
 			// if length is sufficiently different from 1, renormalize
-			if (abs(length2 - 1.f) > c::EPSILON)
-				vec_util::normalize(e, e);
+			// if (abs(length2 - 1.f) > c::EPSILON)
+			vec_util::normalize(e, e);
 		}
 	};
+
+	template<space SPACE, typename T>
+	vec<SPACE> constexpr operator*(T const s, direction<SPACE> const& dir)
+	{
+		const f32 fs = HATS_CAST(f32, s);
+		return vec<SPACE>(dir.x * fs, dir.y * fs, dir.z * fs);
+	}
 } // namespace hats
